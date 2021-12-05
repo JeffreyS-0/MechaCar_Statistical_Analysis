866736349afa7f1afaef048b636dc418b65dcb36
Jeffrey Sykora <jeffsykora1@gmail.com>
2021-12-05 14:49:18 -0600

    Used t.test() function and subset() argument to determine if each manufacturing lot statistically differs from the pop. mean of 1500 PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,6 @@ lot_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean 
 # Deliverable 3
 t.test(suspension_df$PSI, mu = 1500)
 
+t.test(subset(suspension_df, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_df, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_df, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
