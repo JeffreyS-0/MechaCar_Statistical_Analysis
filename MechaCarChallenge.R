380f614c8130d9c8e20886efd33efc85fd8bf63a
Jeffrey Sykora <jeffsykora1@gmail.com>
2021-12-05 14:17:56 -0600

    Read Suspension Coil file into a DF and summarized Mean, Median, Var, StD for PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,6 @@ lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,
 
 summary(lm(mpg~vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_df))
 
+suspension_df <- read.csv(file = '../Desktop/Analytics Class/Module 15_R/MechaCar_Statistical_Analysis/Resources/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- suspension_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
